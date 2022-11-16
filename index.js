cce9d2c3390250433521b056071ab67b580deccb
jonghyeon <bbcjk444@gmail.com>
2022-11-16 15:59:34 +0900

    work 1

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const PORT = process.env.port || 8008;
+const cors = require('cors');
+const iconv = require('iconv-lite');
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let corsOptions = {
+  origin: '*',
+  Credential: true,
+};
+
+app.use(cors(corsOptions));
+
+const db = mysql.createPool({
+  host: 'project-db-stu.ddns.net',
+  user: 'jsGame',
+  password: '123456',
+  database: 'jsGame',
+});
+
+app.listen(PORT, () => {
+  console.log(`running on port ${PORT}`);
+});
