a63798e814c1904db948e5c7579285bf8b887c0b
sping-coding <parkin153@naver.com>
2022-11-16 17:07:09 +0900

    'aaa'

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
 const PORT = process.env.port || 8008;
-const cors = require('cors');
-const iconv = require('iconv-lite');
+const cors = require("cors");
+const iconv = require("iconv-lite");
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let corsOptions = {
-  origin: '*',
+  origin: "*",
   Credential: true,
 };
 
 app.use(cors(corsOptions));
 
 const db = mysql.createPool({
-  host: 'project-db-stu.ddns.net',
-  user: 'jsGame',
-  password: '123456',
-  database: 'jsGame',
+  host: "project-db-stu.ddns.net",
+  user: "jsGame",
+  password: "123456",
+  database: "jsGame",
 });
 
 app.listen(PORT, () => {
   console.log(`running on port ${PORT}`);
+  console.log("aaaa");
+  console.log("bbb");
 });
